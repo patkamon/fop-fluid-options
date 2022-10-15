0405b067bbee20722544dac201914b8a4aa271ef
patkamon <patkamon.a@ku.th>
2022-10-15 20:13:02 +0700

    test action

--- frontend/components/ud-login/UDlogin.jsx
+++ frontend/components/ud-login/UDlogin.jsx
@@ -1,68 +1,68 @@
-import React from 'react'
-import UAuth from '@uauth/js'
+import React from "react";
+import UAuth from "@uauth/js";
 import { Button, Tooltip } from "@nextui-org/react";
-import { useState } from 'react';
-import { ethers } from 'ethers';
-import { Avatar } from '@nextui-org/react';
+import { useState } from "react";
+import { ethers } from "ethers";
+import { Avatar } from "@nextui-org/react";
 import { Text } from "@nextui-org/react";
 
 // this config has to be completely match when congig a provider
 const uauth = new UAuth({
   clientID: "1cf2e143-b474-43df-a001-1a68d77b8a57",
   redirectUri: "https://fop-fluid-options.herokuapp.com/",
-  scope: "openid wallet"
-})
+  scope: "openid wallet",
+});
 
 const UDlogin = () => {
   const [address, setAddress] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
 
   const login = async () => {
-      try {
-        const authorization = await uauth.loginWithPopup()
-        console.log(`address:${authorization.idToken.wallet_address} login`)
-        setIsConnected(true)
-        setAddress(authorization.idToken.wallet_address)
-      } catch (error) {
-        console.error(error)
-      }
+    try {
+      const authorization = await uauth.loginWithPopup();
+      console.log(`address:${authorization.idToken.wallet_address} login`);
+      setIsConnected(true);
+      setAddress(authorization.idToken.wallet_address);
+    } catch (error) {
+      console.error(error);
     }
+  };
 
   const logout = async () => {
-      await uauth.logout()
-      setIsConnected(false)
-      setAddress(null)
-      console.log('Logged out with Unstoppable')
-      }
+    await uauth.logout();
+    setIsConnected(false);
+    setAddress(null);
+    console.log("Logged out with Unstoppable");
+    Router.reload();
+  };
 
-  
   if (!isConnected) {
-
-    return <div className='m-2'>
-      <Button size="sm" color="primary" onPress={login}>
-        <Text color='white' weight="bold">
-          UD connect
-        </Text>
+    return (
+      <div className="m-2">
+        <Button size="sm" color="primary" onPress={login}>
+          <Text color="white" weight="bold">
+            UD connect
+          </Text>
         </Button>
-    </div>
+      </div>
+    );
   }
 
   const addressCheckSummed = ethers.utils.getAddress(address);
-  console.log(addressCheckSummed)
+  console.log(addressCheckSummed);
   return (
-    <div className='flex bg-blue-200 shadow-md p-2 rounded-lg space-x-1'>
-          <Avatar 
-              text="UD" 
-              color="primary" 
-              textColor="white" />
-          <Tooltip content={"log out ?"} rounded color="primary" placement='bottom'>
-            <Button auto flat onPress={logout}>
-              {`${addressCheckSummed.slice(0,5)}...${address.slice(-4,addressCheckSummed.length)}`}
-            </Button>
-          </Tooltip>
+    <div className="flex bg-blue-200 shadow-md p-2 rounded-lg space-x-1">
+      <Avatar text="UD" color="primary" textColor="white" />
+      <Tooltip content={"log out ?"} rounded color="primary" placement="bottom">
+        <Button auto flat onPress={logout}>
+          {`${addressCheckSummed.slice(0, 5)}...${address.slice(
+            -4,
+            addressCheckSummed.length
+          )}`}
+        </Button>
+      </Tooltip>
     </div>
+  );
+};
 
-  )
-}
-
-export default UDlogin
+export default UDlogin;
